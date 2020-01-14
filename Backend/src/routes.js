a8bea3e9a8573f233802f6e6bca14bda08b73ffd
EduardoPicolo <eduardonpicolo@gmail.com>
2020-01-14 17:38:01 -0300

    Implementado cadastro de Devs

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const axios = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
@@ -8,12 +9,23 @@ routes.get('/', (request, response) => {
 });
 
 routes.post('/devs', async(request, response) => {
-    const { github_username } = request.body;
-
+    const { github_username, techs } = request.body;
     const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-    console.log(apiResponse.data);
+    const { name = login , avatar_url, bio} = apiResponse.data;
 
-    return response.json({ message: "Hello world!" });
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    console.log(github_username, name, bio, techs, avatar_url);
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+    });
+
+    return response.json(dev);
 });
 
 module.exports = routes;
