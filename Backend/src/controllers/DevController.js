2317782b1932136b34b36594582d8f498d3e2524
EduardoPicolo <eduardonpicolo@gmail.com>
2020-01-14 19:20:46 -0300

    Implementado busca de Devs por techs e localização

--- Backend/src/controllers/DevController.js
+++ Backend/src/controllers/DevController.js
@@ -1,7 +1,14 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
+    async index(request, response){
+        const devs = await Dev.find();
+
+        return response.json(devs);
+    },
+
     async store(request, response) {
         const { github_username, techs, latitude, longitude } = request.body;
 
@@ -10,7 +17,9 @@ module.exports = {
             const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
             const { name = login, avatar_url, bio } = apiResponse.data;
     
-            const techsArray = techs.split(',').map(tech => tech.trim());
+            // const techsArray = techs.split(',').map(tech => tech.trim());
+            const techsArray = parseStringAsArray(techs);
+
             const location = {
                 type: 'Point',
                 coordinates: [longitude, latitude],
@@ -27,7 +36,6 @@ module.exports = {
                 location,
             });
         }
-
         return response.json(dev);
     }
 };
