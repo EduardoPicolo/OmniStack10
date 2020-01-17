8ffadfd14f42e94b10efc08c9db5b770331a15f1
EduardoPicolo <eduardonpicolo@gmail.com>
2020-01-17 02:17:45 -0300

    Adicionado barra de busca

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Image, View, Text } from "react-native";
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from "react-native";
 import MapView, { Marker, Callout } from "react-native-maps";
 import { requestPermissionsAsync, getCurrentPositionAsync } from "expo-location";
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
@@ -34,21 +35,37 @@ function Main({ navigation }) {
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{ latitude: -15.861178, longitude: -47.980313 }}>
-        <Image style={styles.avatar} source={{ uri: "https://avatars3.githubusercontent.com/u/11791361?v=4" }} ></Image>
+    <>
+      <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{ latitude: -15.861178, longitude: -47.980313 }}>
+          <Image style={styles.avatar} source={{ uri: "https://avatars3.githubusercontent.com/u/11791361?v=4" }} ></Image>
 
-        <Callout onPress={() => {
-          navigation.navigate('Profile', { github_username: 'eduardopicolo' });
-        }}>
-          <View style={styles.callout}>
-            <Text style={styles.devName}>Eduardo Nunes</Text>
-            <Text style={styles.devBio}>Biografia</Text>
-            <Text style={styles.devTechs}>Tecnolagias</Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+          <Callout onPress={() => {
+            navigation.navigate('Profile', { github_username: 'eduardopicolo' });
+          }}>
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Eduardo Nunes</Text>
+              <Text style={styles.devBio}>Biografia</Text>
+              <Text style={styles.devTechs}>Tecnolagias</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar Devs por techs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
+        />
+
+        <TouchableOpacity onPress={() => { }} style={styles.loadButton}>
+          <MaterialIcons name="my-location" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 };
 
@@ -82,6 +99,44 @@ const styles = StyleSheet.create({
   devTechs: {
     marginTop: 5,
   },
+
+  searchForm: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: 'row',
+  },
+
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#FFF',
+    color: '#333',
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    elevation: 2,
+  },
+
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#8E4Dff',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 15,
+    elevation: 2,
+  },
+
 
 });
 
