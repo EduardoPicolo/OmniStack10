8c4770c32fa68e1f19b52b9e342e4eb5d7e295ca
EduardoPicolo <eduardonpicolo@gmail.com>
2020-01-17 01:47:52 -0300

    Adicionado imagem para Devs no mapa e navegação para perfil

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import { StyleSheet, Image, View, Text } from "react-native";
+import MapView, { Marker, Callout } from "react-native-maps";
 import { requestPermissionsAsync, getCurrentPositionAsync } from "expo-location";
 
-function Main() {
+function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
 
   useEffect(() => {
@@ -33,13 +33,56 @@ function Main() {
     return null;
   }
 
-  return <MapView initialRegion={currentRegion} style={styles.map} />;
-}
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{ latitude: -15.861178, longitude: -47.980313 }}>
+        <Image style={styles.avatar} source={{ uri: "https://avatars3.githubusercontent.com/u/11791361?v=4" }} ></Image>
+
+        <Callout onPress={() => {
+          navigation.navigate('Profile', { github_username: 'eduardopicolo' });
+        }}>
+          <View style={styles.callout}>
+            <Text style={styles.devName}>Eduardo Nunes</Text>
+            <Text style={styles.devBio}>Biografia</Text>
+            <Text style={styles.devTechs}>Tecnolagias</Text>
+          </View>
+        </Callout>
+      </Marker>
+    </MapView>
+  );
+};
 
 const styles = StyleSheet.create({
   map: {
     flex: 1,
   },
+
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: '#FFF',
+  },
+
+  callout: {
+    width: 260,
+  },
+
+  devName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+
+  devBio: {
+    color: '#666',
+    marginTop: 5,
+  },
+
+  devTechs: {
+    marginTop: 5,
+  },
+
 });
 
 export default Main;
