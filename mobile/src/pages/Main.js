fc169827827bc36ed107e0052484941111c13ba3
EduardoPicolo <eduardonpicolo@gmail.com>
2020-01-17 03:05:25 -0300

    Implementado busca na api

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -4,9 +4,13 @@ import MapView, { Marker, Callout } from "react-native-maps";
 import { requestPermissionsAsync, getCurrentPositionAsync } from "expo-location";
 import { MaterialIcons } from '@expo/vector-icons';
 
+import api from '../services/api';
+
 function Main({ navigation }) {
+  const [devs, setDevs] = useState([]);
   const [currentRegion, setCurrentRegion] = useState(null);
 
+  //Localizar usuario
   useEffect(() => {
     async function loadInitialPosition() {
       const { granted } = await requestPermissionsAsync();
@@ -30,26 +34,54 @@ function Main({ navigation }) {
     loadInitialPosition();
   }, []);
 
+  async function loadDevs() {
+    const { latitude, longitude } = currentRegion;
+    const response = await api.get('/search', {
+      params: {
+        latitude,
+        longitude,
+        techs: 'ReactJS',
+      }
+    });
+    setDevs(response.data);
+  };
+
+  function handleRegionChange(region) {
+    setCurrentRegion(region);
+  };
+
   if (!currentRegion) {
     return null;
   }
 
   return (
     <>
-      <MapView initialRegion={currentRegion} style={styles.map}>
-        <Marker coordinate={{ latitude: -15.861178, longitude: -47.980313 }}>
-          <Image style={styles.avatar} source={{ uri: "https://avatars3.githubusercontent.com/u/11791361?v=4" }} ></Image>
-
-          <Callout onPress={() => {
-            navigation.navigate('Profile', { github_username: 'eduardopicolo' });
+      <MapView
+        initialRegion={currentRegion}
+        onRegionChangeComplete={handleRegionChange}
+        style={styles.map}
+      >
+        {devs.map(dev => (
+          <Marker key={dev._id} coordinate={{
+            longitude: dev.location.coordinates[0],
+            latitude: dev.location.coordinates[1],
           }}>
-            <View style={styles.callout}>
-              <Text style={styles.devName}>Eduardo Nunes</Text>
-              <Text style={styles.devBio}>Biografia</Text>
-              <Text style={styles.devTechs}>Tecnolagias</Text>
-            </View>
-          </Callout>
-        </Marker>
+            <Image
+              style={styles.avatar}
+              source={{ uri: dev.avatar_url }}
+            />
+
+            <Callout onPress={() => {
+              navigation.navigate('Profile', { github_username: dev.github_username });
+            }}>
+              <View style={styles.callout}>
+                <Text style={styles.devName}>{dev.name}</Text>
+                <Text style={styles.devBio}>{dev.bio}</Text>
+                <Text style={styles.devTechs}>{dev.techs.join(', ')}</Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
       </MapView>
 
       <View style={styles.searchForm}>
@@ -61,7 +93,7 @@ function Main({ navigation }) {
           autoCorrect={false}
         />
 
-        <TouchableOpacity onPress={() => { }} style={styles.loadButton}>
+        <TouchableOpacity onPress={loadDevs} style={styles.loadButton}>
           <MaterialIcons name="my-location" size={20} color="#FFF" />
         </TouchableOpacity>
       </View>
