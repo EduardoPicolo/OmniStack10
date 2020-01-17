520e852189aebfc9466b5f6e91df9b7d75fd8266
EduardoPicolo <eduardonpicolo@gmail.com>
2020-01-17 00:30:07 -0300

    App mobile criado

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+
+function Main() {
+    useEffect(() => {
+        async function loadInitialPosition() {
+            const { granted } = await requestPermissionsAsync();
+            if (granted) {
+                const location = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+            };
+        };
+
+    }, []);
+
+    return <MapView style={styles.map} />
+}
+
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1,
+    },
+});
+
+export default Main;
